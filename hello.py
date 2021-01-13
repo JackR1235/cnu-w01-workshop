c3beaaeb94b22f6036aa45b01dac28d014b25393
JackR1235 <s1903977@ed.ac.uk>
2021-01-13 11:07:41 +0000

    Added my birthday to hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-name = 'Josephine'
-age = 36
-
+name = 'Jack'
+age = 19
+birthday = '11th July'
 print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'My birthday is on the {birthday}.')
